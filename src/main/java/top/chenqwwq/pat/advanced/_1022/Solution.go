5558bcfa69b4bce7fb1a9dd73c8d57aa3322759e
chenqwwq <chenbxxx@gmail.com>
2021-08-27 15:35:43 +0800

    sync

--- src/main/java/top/chenqwwq/pat/advanced/_1022/Solution.go
+++ src/main/java/top/chenqwwq/pat/advanced/_1022/Solution.go
@@ -15,52 +15,59 @@ import (
 */
 
 func main() {
-	var n int
-	_, _ = Scanf("%d\n", &n)
-	dict := make([]map[string]map[int]bool, 5)
-	for i := 0; i < 5; i++ {
-		dict[i] = make(map[string]map[int]bool)
-	}
-	add := func(dict map[string]map[int]bool, key string, value int) {
-		if dict[key] == nil {
-			dict[key] = make(map[int]bool)
-		}
-		dict[key][value] = true
-	}
 	reader := bufio.NewReader(os.Stdin)
 	scanf := func() string {
 		value, _ := reader.ReadString('\n')
 		return strings.Replace(value, "\n", "", -1)
 	}
+	// n个数据
+	n, _ := strconv.Atoi(scanf())
+	// 保存数据,go 只能使用 map 实现 set 的功能
+	// 0 - 标题
+	// 1 - 作者名称
+	// 2 - 关键字
+	// 3 - 出版社
+	// 4 - 发行年份
+	dict := make([]map[string]map[string]bool, 5)
+	for i := 0; i < 5; i++ {
+		dict[i] = make(map[string]map[string]bool)
+	}
+	// 添加方法
+	add := func(hash map[string]map[string]bool, key, id string) {
+		if hash[key] == nil {
+			hash[key] = make(map[string]bool)
+		}
+		hash[key][id] = true
+	}
 	for i := 0; i < n; i++ {
-		id, _ := strconv.Atoi(scanf())
+		id := scanf()
 		add(dict[0], scanf(), id)
 		add(dict[1], scanf(), id)
-		k := scanf()
-		for _, key := range strings.Split(k, " ") {
+		for _, key := range strings.Split(scanf(), " ") {
 			add(dict[2], key, id)
 		}
 		add(dict[3], scanf(), id)
 		add(dict[4], scanf(), id)
 	}
+
+	// m 个查询
 	m, _ := strconv.Atoi(scanf())
-	printSet := func(hash map[int]bool) {
-		var keys []int
-		for k,_ := range hash {
+	printSet := func(hash map[string]bool) {
+		var keys []string
+		for k := range hash {
 			keys = append(keys, k)
 		}
-		sort.Ints(keys)
+		sort.Strings(keys)
 		for _, v := range keys {
 			Println(v)
 		}
 	}
 	for i := 1; i <= m; i++ {
 		line := scanf()
-		split := strings.Split(line, ":")
+		split := strings.Split(line, ": ")
 		_type, _ := strconv.Atoi(split[0])
-		key := strings.TrimSpace(split[1])
 		Println(line)
-		if hash, exist := dict[_type-1][key]; exist {
+		if hash, exist := dict[_type-1][split[1]]; exist {
 			printSet(hash)
 		} else {
 			Println("Not Found")
