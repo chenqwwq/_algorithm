5332627234df596c4a63cfeb4ead8d07b89b7f14
chenqwwq <chenbxxx@gmail.com>
2021-09-01 11:15:33 +0800

    sync

---
+++ src/main/java/top/chenqwwq/leetcode/hot100/_42/Solution.java
@@ -0,0 +1,42 @@
+package top.chenqwwq.leetcode.hot100._42;
+
+import java.util.Stack;
+
+/**
+ * @author chen
+ * @date 2021-09-01
+ **/
+public class Solution {
+
+	public int trap(int[] height) {
+		// 单调栈 栈中保存的是元素的下标
+		// 维护栈中元素整体递减
+		final int n = height.length;
+		Stack<Integer> stack = new Stack<>();
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
+				final Integer pre = stack.pop();
+				ans += 
+			}
+			stack.push(i);
+		}
+
+	}
+//	public int trap(int[] height) {
+//		final int n = height.length;
+//		int[] leftMax = new int[n], rightMax = new int[n];
+//		leftMax[0] = height[0];
+//		rightMax[n - 1] = height[n - 1];
+//		for (int i = 1; i < n - 1; i++) {
+//			leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+//			rightMax[n - i - 1] = Math.max(rightMax[n - i], height[n - 1 - i]);
+//		}
+//
+//		int ans = 0;
+//		for (int i = 1; i < n - 1; i++) {
+//			ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+//		}
+//		return ans;
+//	}
+}
