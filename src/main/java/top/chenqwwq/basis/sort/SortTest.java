b4f89cc23ab5d52fe48258cea29acb26fd8c8a82
chenqwwq <chenbxxx@gmail.com>
2021-09-30 07:38:28 +0800

    sync

--- src/main/java/top/chenqwwq/basis/sort/SortTest.java
+++ src/main/java/top/chenqwwq/basis/sort/SortTest.java
@@ -12,7 +12,7 @@ class SortTest {
 	@Test
 	void testSortAes() {
 		int[] ints = {10, 8, 2, 5, 7, 3, 9, 6, 1, 4};
-		new MergeSort().sortAes(ints);
+		new QuickSort().sortAes(ints);
 		Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, ints);
 	}
 }
