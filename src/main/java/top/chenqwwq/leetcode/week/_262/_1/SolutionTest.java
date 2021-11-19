6ade4eddfbf9a6df5ee6a9faa6131b955a166fbf
chenqwwq <chenbxxx@gmail.com>
2021-11-20 00:59:24 +0800

    sync

---
+++ src/main/java/top/chenqwwq/leetcode/week/_262/_1/SolutionTest.java
@@ -0,0 +1,18 @@
+package top.chenqwwq.leetcode.week._262._1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author chen
+ * @date 2021-11-19
+ **/
+class SolutionTest {
+
+	@Test
+	void countVowelSubstrings() {
+		final Solution solution = new Solution();
+		solution.countVowelSubstrings("cuaieuouac");
+	}
+}
