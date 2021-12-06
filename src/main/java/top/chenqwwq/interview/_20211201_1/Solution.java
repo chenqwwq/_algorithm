272646c9914fd18a002c409d574f59f8ee2e822a
chenqwwq <17826875823@100fen.cn>
2021-12-06 15:09:08 +0800

    同步一哈

---
+++ src/main/java/top/chenqwwq/interview/_20211201_1/Solution.java
@@ -0,0 +1,49 @@
+package top.chenqwwq.interview._20211201_1;
+
+import top.chenqwwq.leetcode.common.ListNode;
+
+import java.util.List;
+
+/**
+ * @author chenbingxin
+ * @date 2021/12/1
+ **/
+public class Solution {
+	// 1 2 3 2 1
+	public boolean isPalindrome(ListNode head) {
+		if(head == null){
+			return true;
+		}
+		ListNode slow = head, fast = head;
+		while (fast != null && fast.next != null){
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		ListNode half = reorder(slow);
+		while (half != null && head != null){
+			if(head.val != half.val){
+				return false;
+			}
+			head = head.next;
+			half = half.next;
+		}
+		return true;
+	}
+
+	public ListNode reorder(ListNode node){
+		// 3 2 1
+		ListNode pre = node,curr = node.next;
+		pre.next = null;
+		while (curr != null){
+			ListNode next = curr.next;
+			curr.next = pre;
+			pre = curr;
+			curr = next;
+		}
+		return pre;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(ClassLoader.getSystemClassLoader().getClass().getSimpleName());
+	}
+}
---
+++ src/main/java/top/chenqwwq/leetcode/week/_270/_1/Solution.java
@@ -0,0 +1,54 @@
+package top.chenqwwq.leetcode.week._270._1;
+
+import java.util.*;
+
+/**
+ * @author chenbingxin
+ * @date 2021/12/5
+ **/
+public class Solution {
+	public int[] findEvenNumbers(int[] digits) {
+		List<Integer> first = new ArrayList<>(), last = new ArrayList<>();
+		for (int i = 0; i < digits.length; i++) {
+			if (digits[i] != 0) {
+				first.add(i);
+			}
+			if ((digits[i] & 1) == 0) {
+				last.add(i);
+			}
+		}
+
+		final Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < first.size(); i++) {
+			for (int j = 0; j < digits.length; j++) {
+				if(first.get(i) == j){
+					continue;
+				}
+				for (int k = 0; k < last.size(); k++) {
+					if(Objects.equals(first.get(i), last.get(k)) || j == last.get(k)){
+						continue;
+					}
+					set.add(digits[first.get(i)] * 100 + digits[j] * 10 + digits[last.get(k)]);
+				}
+			}
+		}
+		List<Integer> ans = new ArrayList<>(set);
+		ans.sort(Integer::compare);
+		int[] fin = new int[ans.size()];
+		for (int i = 0;i <  ans.size();i++){
+			fin[i] = ans.get(i);
+		}
+		return fin;
+	}
+
+	public static void main(String[] args) {
+		new Solution().findEvenNumbers(new int[]{0,0,2,0});
+	}
+}
+/**
+ *[102,132,201,231,301,302,312,321]
+ * [222,228,282,288,822,828,882]
+ * []
+ * [0,2,20]
+ * []
+ */
