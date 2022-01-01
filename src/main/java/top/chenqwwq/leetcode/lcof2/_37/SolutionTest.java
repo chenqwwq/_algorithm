fa063713c012aa6a9445101d0f1063d6b0118112
chenqwwq <17826875823@100fen.cn>
2022-01-01 23:14:04 +0800

    同步一哈

---
+++ src/main/java/top/chenqwwq/leetcode/lcof2/_37/SolutionTest.java
@@ -0,0 +1,22 @@
+package top.chenqwwq.leetcode.lcof2._37;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author chenbingxin
+ * @date 2022/1/1
+ **/
+class SolutionTest {
+
+    @Test
+    void asteroidCollision() {
+        Solution solution = new Solution();
+        Assertions.assertEquals(new int[]{-2,-1,-2},solution.asteroidCollision(new int[]{-2,-1,1,-2}));
+        Assertions.assertEquals(new int[]{10},solution.asteroidCollision(new int[]{10,2,-5}));
+        Assertions.assertEquals(new int[]{-2,-2,-2},solution.asteroidCollision(new int[]{-2,-2,1,-2}));
+        Assertions.assertEquals(new int[]{},solution.asteroidCollision(new int[]{8,-8}));
+    }
+}
