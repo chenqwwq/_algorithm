29df72881939c1217007544c994c3dce4f857410
沽酒 <chenbingxin@cai-inc.com>
2022-07-12 20:33:26 +0800

    update

---
+++ src/top.chenqwwq/leetcode/daily/_20220712/Solution.java
@@ -0,0 +1,22 @@
+package top.chenqwwq.leetcode.daily._20220712;
+
+/**
+ * @author 沽酒
+ * @since 2022/07/12
+ */
+public class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int[] a = new int[m], b = new int[n];
+        for (int[] idx : indices) {
+            a[idx[0]]++;
+            b[idx[1]]++;
+        }
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += (a[i] + b[j]) & 1;
+            }
+        }
+        return ans;
+    }
+}
