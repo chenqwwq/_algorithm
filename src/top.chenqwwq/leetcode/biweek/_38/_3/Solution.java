23ddfc00fd04e667ac96ae713bf8b77eff550fb0
沽酒 <chenbingxin@cai-inc.com>
2022-07-31 23:33:19 +0800

    update

--- src/top.chenqwwq/leetcode/biweek/_38/_3/Solution.java
+++ src/top.chenqwwq/leetcode/biweek/_38/_3/Solution.java
@@ -6,17 +6,6 @@ package top.chenqwwq.leetcode.biweek._38._3;
  **/
 public class Solution {
     public int countSubstrings(String s, String t) {
-        int n = s.length(), m = t.length();
-        int ans = 0;
-        // 长度
-        for (int l = 1; l <= n; l++) {
-            // 起始地址
-            for (int i = l; i < t.length(); i++) {
-                int j = i + l;
-
-            }
-        }
-
-        return ans;
+        return 0;
     }
 }
