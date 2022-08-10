e41151265583e5f3cf0ff492268f5ca9d9dc6784
沽酒 <chenbingxin@cai-inc.com>
2022-08-10 13:46:26 +0800

    update

---
+++ src/top.chenqwwq/leetcode/daily/_20220805/Solution.java
@@ -0,0 +1,27 @@
+package top.chenqwwq.leetcode.daily._20220805;
+
+import top.chenqwwq.leetcode.common.TreeNode;
+
+/**
+ * @author 沽酒
+ * @since 2022/08/05
+ */
+public class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        return dfs(root, 1, val, depth, false);
+    }
+
+    public TreeNode dfs(TreeNode node, int curr, int val, int depth, boolean right) {
+        if (curr < depth) {
+            if (node != null) {
+                node.left = dfs(node.left, curr + 1, val, depth, false);
+                node.right = dfs(node.right, curr + 1, val, depth, true);
+            }
+            return node;
+        }
+        final TreeNode ret = new TreeNode(val);
+        if (right) ret.right = node;
+        else ret.left = node;
+        return ret;
+    }
+}
